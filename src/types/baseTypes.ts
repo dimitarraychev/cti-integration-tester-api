cb1c6aabde05ecf45a3a6f2fbd47c27eb9158c93
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-07-01 08:39:31 +0000

    Added base types

---
+++ src/types/baseTypes.ts
@@ -0,0 +1,15 @@
+export interface BasePayload {
+  command: string;
+  amount?: number;
+  bet_amount?: number;
+  win_amount?: number;
+  command_to_fail?: string;
+}
+
+export interface BaseResponse {
+  currency: string;
+  response_message: string;
+  response_code: string;
+  totalbalance?: number;
+  freeround_limit?: number;
+}
