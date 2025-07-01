6e47832623e8c9840ba9c674585d60cfc277ee28
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-07-01 08:39:24 +0000

    Added base controller

---
+++ src/controllers/baseController.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import { generateLogsBase } from "../utils/logGenerators";
+import { BasePayload, BaseResponse } from "../types/baseTypes";
+
+let totalBalance = 123456;
+let simulateError = false;
+let commandToFail = "";
+const errorResponseMessage = "temporary_error";
+
+const response: BaseResponse = {
+  currency: "EUR",
+  response_message: "ok",
+  response_code: "ok",
+};
+
+const resetResponse = () => {
+  response.response_code = "ok";
+  simulateError = false;
+  commandToFail = "";
+};
+
+export const baseHandler = (req: Request, res: Response): void => {
+  try {
+    const payload: BasePayload = req.body.payload_json
+      ? JSON.parse(req.body.payload_json)
+      : req.body;
+
+    const {
+      command,
+      amount = 0,
+      bet_amount = 0,
+      win_amount = 0,
+      command_to_fail,
+    } = payload;
+
+    switch (command) {
+      case "simulate_error":
+        simulateError = true;
+        commandToFail = command_to_fail || "";
+        break;
+
+      case "get_account_balance":
+        response.totalbalance = totalBalance;
+        break;
+
+      case "add_account_game_bet":
+        totalBalance -= amount;
+        response.totalbalance = totalBalance;
+        break;
+
+      case "add_account_game_win":
+        totalBalance += amount;
+        response.totalbalance = totalBalance;
+        break;
+
+      case "add_account_game_bet_and_win":
+        totalBalance -= bet_amount;
+        totalBalance += win_amount;
+        response.totalbalance = totalBalance;
+        break;
+
+      case "cancel":
+        totalBalance += amount;
+        response.freeround_limit = 0;
+        response.totalbalance = totalBalance;
+        break;
+
+      default:
+        console.log("Unknown command received:", command);
+        res.status(400).json({ message: "Unknown command" });
+        return;
+    }
+
+    response.response_code =
+      simulateError && commandToFail === command ? errorResponseMessage : "ok";
+
+    generateLogsBase(
+      req.headers,
+      payload,
+      command,
+      simulateError,
+      commandToFail,
+      response
+    );
+
+    res.json(response);
+
+    if (simulateError && commandToFail === command) {
+      resetResponse();
+    }
+  } catch (error) {
+    console.error("Base API Error:", error);
+    res.status(400).json({ message: "Invalid JSON format" });
+  }
+};
