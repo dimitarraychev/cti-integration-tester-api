960dd3d31ac457aacbf6d442aed03e2b9b7f4eae
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-09-18 10:58:55 +0000

    added iSoftBet

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { Express } from "express";
 import cors from "cors";
 
 import { log } from "./middlewares/log.js";
-import baseController from './controllers/baseController.js'
+import baseController from "./controllers/baseController.js";
+import iSoftBetController from "./controllers/iSoftBetController.js";
 
 const app: Express = express();
 const PORT: number | string = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(log);
 
 app.use("/base", baseController);
+app.use("/isoftbet", iSoftBetController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
