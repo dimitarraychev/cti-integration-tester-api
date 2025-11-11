4ef02a0647017eb01df65760d7dd90786712555d
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-11-11 14:29:22 +0000

    fix2

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app: Express = express();
 const PORT: number | string = process.env.PORT || 3000;
 
 app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
