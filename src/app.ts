80b3393924d64ee592f58182a52d8d56bbf241b0
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-11-11 14:40:04 +0000

    cors

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,7 @@ import softswissV2Controller from "./controllers/softswissV2Controller.js";
 const app: Express = express();
 const PORT: number | string = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-request-sign"],
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
