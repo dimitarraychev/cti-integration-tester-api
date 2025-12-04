474e6a78277cde09eac3dc3ae1782ce3385e350e
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-12-04 11:26:58 +0000

    further fix

--- src/services/softswissV2Service.ts
+++ src/services/softswissV2Service.ts
@@ -3,18 +3,18 @@ import crypto from "crypto";
 
 let totalBalance = 123456;
 
-export const generateHmacSha256Hex = (payload: string, PSK: string): string => {
-  return crypto.createHmac("sha256", PSK).update(payload, "utf8").digest("hex");
+export const generateHmacSha256Hex = (payload: string, psk: string): string => {
+  return crypto.createHmac("sha256", psk).update(payload, "utf8").digest("hex");
 };
 
 export const generateSoftswissLaunchURL = async (
   req: Request,
   res: Response
 ) => {
-  const { formData, apikey, psk } = req.body;
+  const { payload, apikey, psk } = req.body;
   const baseUrl = "https://rgs-stage-api.ctrgs.com:1381/softswiss-api/";
   const url = `${baseUrl}${apikey}/v2/a8r_provider.Launcher/Real`;
-  const signature = generateHmacSha256Hex(formData, psk);
+  const signature = generateHmacSha256Hex(payload, psk);
 
   try {
     const response = await fetch(url, {
@@ -23,13 +23,14 @@ export const generateSoftswissLaunchURL = async (
         "Content-Type": "application/json",
         "X-REQUEST-SIGN": signature,
       },
-      body: JSON.stringify(JSON.parse(formData)),
+      body: payload,
     });
 
-    console.log("RES: ", response);
+    console.log("RESPONSE FROM RGS: ", response);
 
     if (!response.ok) {
-      return res.status(400).json({ message: "Internal Error" });
+      res.status(400).json({ message: "Internal Error" });
+      return;
     }
 
     const data = await response.json();
