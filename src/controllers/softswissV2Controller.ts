c34455bdba2dff1e0d879ae0dd32041038d6897d
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2025-12-04 11:28:58 +0000

    fix2

--- src/controllers/softswissV2Controller.ts
+++ src/controllers/softswissV2Controller.ts
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-  generateSoftswissLaunchURL,
-  softswissV2Service,
-} from "../services/softswissV2Service.js";
+import { generateSoftswissLaunchURL } from "../services/softswissV2Service.js";
 
 const router = express.Router();
 
-router.post("/", softswissV2Service);
 router.post("/launch", generateSoftswissLaunchURL);
 
 export default router;
